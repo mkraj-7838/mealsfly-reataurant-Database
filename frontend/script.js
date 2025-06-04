6123b651434b367497b0fdd289cb6d8597d4b993
Mohit Prajapati <221210070@nitdelhi.ac.in>
2025-06-04 23:44:14 +0530

    link

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://mealsfly-reataurant-database.onrender.com/api';
 
 // DOM Elements
 const homepage = document.getElementById('homepage');
